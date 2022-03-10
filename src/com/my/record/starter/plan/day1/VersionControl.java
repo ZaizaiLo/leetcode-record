07ccf1098057bee2895479d7d8298d4ab85de38b
luojiyuan <893049558@qq.com>
2022-03-10 22:14:19 +0800

    20天提交计划     版本控制

---
+++ src/com/my/record/starter/plan/day1/VersionControl.java
@@ -0,0 +1,28 @@
+package com.my.record.starter.plan.day1;
+
+/**
+ * @description
+ * @author luojiyuan
+ * @date 2022-03-10 21:59
+ */
+
+public class VersionControl {
+
+    public int firstBadVersion(int n) {
+        int left = 1;
+        while (left < n) {
+            int mid = left + (n - left) / 2;
+            if (isBadVersion(mid)) {
+                n = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean isBadVersion(int i) {
+        return true;
+    }
+
+}
