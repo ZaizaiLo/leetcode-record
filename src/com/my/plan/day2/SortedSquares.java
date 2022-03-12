c2fb7ef2969fb557fc0e5036138a6fdbd334fea0
luojiyuan <893049558@qq.com>
2022-03-12 12:34:20 +0800

    20天提交计划     有序数的平方

---
+++ src/com/my/plan/day2/SortedSquares.java
@@ -0,0 +1,20 @@
+package com.my.plan.day2;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * @description 有序数的平方
+ * @author luojiyuan
+ * @date 2022-03-12 12:28
+ */
+public class SortedSquares {
+
+    public int[] sortedSquares(int[] nums) {
+        IntStream sorted = Arrays.stream(nums).sorted();
+        return sorted.map(item -> {
+            item = item * item;
+            return item;
+        }).sorted().toArray();
+    }
+}
